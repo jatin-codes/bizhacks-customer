7116d51dd58b458784758c2607f2e3f61000cd15
JATIN <jatin@metaoptima.com>
2019-02-02 04:04:31 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import PartnerMain from './components/PartnerMain';
 
 class App extends Component {
   render() {
     return (
-      <div className="">
+      <div>
+        <BrowserRouter>
+          <Route exact path="/" component= {PartnerMain}/>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/components/PartnerMain.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PartnerMain extends Component {
+    render (){
+        return (
+            <div>
+                <h2>Partner Main component</h2>
+            </div>
+        )
+    }
+}
+
+export default PartnerMain;
